0b857440245442c95b305b8c451cbe3bab2e9d4f
Adrian Guzman <adriangbetancur@gmail.com>
2020-01-19 15:16:15 -0400

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('./config/config');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario');
+});
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+
+        res.json({
+            persona: body
+        });
+    }
+
+});
+app.put('/usuario/:id', function(req, res) {
+
+    let userId = req.params.id;
+    res.json({
+        id: userId
+    });
+});
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ',
+        process.env.PORT);
+})
