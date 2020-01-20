2423176f1e5abeff74c41ad779e5c55e4d4352ea
Adrian Guzman <adriangbetancur@gmail.com>
2020-01-19 22:31:04 -0400

    se termino las cuatro peticiones

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -43,28 +43,93 @@ app.post('/usuario', function(req, res) {
 
     let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+
+    usuario.save((err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
 
         res.json({
-            persona: body
+            ok: true,
+            usuario: usuarioDB
         });
-    }
+
+
+    });
+
 
 });
+
 app.put('/usuario/:id', function(req, res) {
 
-    let userId = req.params.id;
-    res.json({
-        id: userId
-    });
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+    })
+
 });
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
+
+app.delete('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+    let cambiaEstado = {
+        estado: false
+    };
+
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+
+    });
 });
 
 module.exports = app;
