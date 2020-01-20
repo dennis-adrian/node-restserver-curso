b53f4a70e66c1862fe834e76abe15f1115f7b781
Adrian Guzman <adriangbetancur@gmail.com>
2020-01-19 21:48:59 -0400

    otra vez, config de conexiones con DB

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,23 @@
-//===============================
-//             Puerto
-//===============================
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://dbAdmin:adminPassw0rd@cluster0-iswmj.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', {
+mongoose.connect(process.env.URLDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, (error, res) => {
