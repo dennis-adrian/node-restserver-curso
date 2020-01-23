174d1484fa0645f51f9d914a5bbd9fe70c37d23b
Adrian Guzman <adriangbetancur@gmail.com>
2020-01-23 18:25:39 -0400

    cambios

--- server/server.js
+++ server/server.js
@@ -3,17 +3,20 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
+
+//fix mongoose errors
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true)
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./routes/usuario'));
 
-mongoose.connect(process.env.URLDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, (error, res) => {
+mongoose.connect(process.env.URLDB, (error, res) => {
 
     if (error) throw error;
 
