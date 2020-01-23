930db4fa164e32a5903b3e0ea6854ac5d9139f35
Adrian Guzman <adriangbetancur@gmail.com>
2020-01-23 19:50:20 -0400

    mensaje

--- server/config/config.js
+++ server/config/config.js
@@ -15,9 +15,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://dbAdmin:adminPassw0rd@cluster0-iswmj.mongodb.net/cafe?retryWrites=true&w=majority';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://dbAdmin:adminPassw0rd@cluster0-iswmj.mongodb.net/cafe?retryWrites=true&w=majority';
+}
 process.env.URLDB = urlDB;
